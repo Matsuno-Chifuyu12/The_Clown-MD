3841950ba818465e2180c6f0cbff68578429de27
MatsunoChifuyu <matsunochifuyuaogiri@gmail.com>
2025-09-24 00:04:21 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const TELEGRAM_BOT_TOKEN = ''; // bot telegram token
+
+export const REDIRECT_BOT = "None" // a redirect bot when the bot is full
+
+export const OWNER_ID = "" // owner id
+
+export const LIMIT = 30; //number of max sessions for the tele bot
+
+export const MODE = "Default";
+
