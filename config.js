869293d9903580465aed18f9f02f7aaed5c5f04b
MatsunoChifuyu <matsunochifuyuaogiri@gmail.com>
2025-09-28 14:00:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 export const TELEGRAM_BOT_TOKEN = ''; // bot telegram token
 
-export const REDIRECT_BOT = "None" // a redirect bot when the bot is full
+export const REDIRECT_BOT = "" // a redirect bot when the bot is full
 
 export const OWNER_ID = "" // owner id
 
