3dd1063723d3ee3115cc7e4809d54a549f70d718
MatsunoChifuyu <matsunochifuyuaogiri@gmail.com>
2025-11-02 22:28:27 +0100

    Update baileys-wrapper.js

--- baileys-wrapper.js
+++ baileys-wrapper.js
@@ -1,2 +1,26 @@
 // baileys-wrapper.js
-export * from '@whiskeysockets/baileys';
+import baileys from '@whiskeysockets/baileys';
+
+export const {
+  downloadMediaMessage,
+  makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+  makeCacheableSignalKeyStore,
+  makeInMemoryStore,
+  delay,
+  getAggregateVotesInPollMessage,
+  proto,
+  generateWAMessageFromContent,
+  generateWAMessage,
+  prepareWAMessageMedia,
+  areJidsSameUser,
+  isJidUser,
+  isJidGroup,
+  isJidBroadcast,
+  jidNormalizedUser,
+  getContentType
+} = baileys;
+
+export default baileys;
